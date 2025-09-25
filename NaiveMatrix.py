612da151aa9644b6d80d601a9e81ae08f636c843
zhouAthalia <65040636+zhouAthalia@users.noreply.github.com>
2025-09-24 18:41:30 -0700

    Update NaiveMatrix.py
    Put the matrix function into helper.py +imported and updated code as needed

--- NaiveMatrix.py
+++ NaiveMatrix.py
@@ -1,34 +1,36 @@
-import random
-from typing import List
+#import random
+#from typing import List
 
-Matrix = List[List[int]]
+from Helper import Matrix, add_matrix, split_matrix, combine_quadrants, createMatrix
+
+#Matrix = List[List[int]]
 
 # -------------------
 # Matrix Generator
 # -------------------
-def matrixGenerator(n: int) -> Matrix:
-    return [[random.randint(-10, 10) for _ in range(n)] for _ in range(n)]
+#def matrixGenerator(n: int) -> Matrix:
+#    return [[random.randint(-10, 10) for _ in range(n)] for _ in range(n)]
 
 # -------------------
 # Helper functions
 # -------------------
-def add_matrix(A: Matrix, B: Matrix) -> Matrix:
-    n = len(A)
-    return [[A[i][j] + B[i][j] for j in range(n)] for i in range(n)]
+#def add_matrix(A: Matrix, B: Matrix) -> Matrix:
+#    n = len(A)
+#    return [[A[i][j] + B[i][j] for j in range(n)] for i in range(n)]
 
-def split_matrix(A: Matrix):
-    n = len(A)
-    mid = n // 2
-    A11 = [row[:mid] for row in A[:mid]]
-    A12 = [row[mid:] for row in A[:mid]]
-    A21 = [row[:mid] for row in A[mid:]]
-    A22 = [row[mid:] for row in A[mid:]]
-    return A11, A12, A21, A22
+#def split_matrix(A: Matrix):
+#    n = len(A)
+#    mid = n // 2
+#    A11 = [row[:mid] for row in A[:mid]]
+#    A12 = [row[mid:] for row in A[:mid]]
+#    A21 = [row[:mid] for row in A[mid:]]
+#    A22 = [row[mid:] for row in A[mid:]]
+#     return A11, A12, A21, A22
 
-def combine_quadrants(C11: Matrix, C12: Matrix, C21: Matrix, C22: Matrix) -> Matrix:
-    top = [c11 + c12 for c11, c12 in zip(C11, C12)]
-    bottom = [c21 + c22 for c21, c22 in zip(C21, C22)]
-    return top + bottom
+# def combine_quadrants(C11: Matrix, C12: Matrix, C21: Matrix, C22: Matrix) -> Matrix:
+#     top = [c11 + c12 for c11, c12 in zip(C11, C12)]
+#     bottom = [c21 + c22 for c21, c22 in zip(C21, C22)]
+#     return top + bottom
 
 # -------------------
 # Naive Divide & Conquer
@@ -67,8 +69,8 @@ def dac_multiply(A: Matrix, B: Matrix) -> Matrix:
 # -------------------
 if __name__ == "__main__":
     n = 4  # must be a power of 2
-    A = matrixGenerator(n)
-    B = matrixGenerator(n)
+    A = createMatrix(n)
+    B = createMatrix(n)
     
     print("Matrix A:")
     for row in A: print(row)
